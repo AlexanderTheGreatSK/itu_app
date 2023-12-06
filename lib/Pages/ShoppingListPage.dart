666f6ba4a3bd8b81e6c95ffd4ae3187129a011a1
Alena Klimecká <alena.klimecka@gmail.com>
2023-12-06 16:31:52 +0100

    Back arrow is gone

--- lib/Pages/ShoppingListPage.dart
+++ lib/Pages/ShoppingListPage.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:itu_app/Database/DataClasses/ShoppingList.dart';
 import 'package:itu_app/Pages/FamilyListPage.dart';
-import 'package:itu_app/Pages/MyListPage.dart';
 import 'package:itu_app/Pages/ListOverviewPage.dart';
 import 'package:itu_app/Widgets/ListsWidget.dart';
 import 'package:itu_app/Database/DatabaseHandler.dart';
@@ -15,47 +14,43 @@ class MyShoppingListPage extends StatefulWidget {
 
 class _MyShoppingListPageState extends State<MyShoppingListPage>
     with TickerProviderStateMixin {
-  late final TabController _tabController;
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(initialIndex: 0, length: 2, vsync: this);
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        bottom: TabBar(
-          controller: _tabController,
-          tabs: const <Widget>[
-            Tab(
-              text: "FAMILY LISTS",
+    return DefaultTabController(
+      initialIndex: 0,
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          leading: Container(),
+          bottom: const TabBar(
+            labelColor: Colors.purple,
+            tabs:  <Widget>[
+              Tab(text: "FAMILY LISTS"),
+              Tab(text: "MY LISTS"),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: <Widget>[
+            Center(
+              child: FamilyListPage(),
             ),
-            Tab(
-              text: "MY LISTS",
+            Center(
+              child: MyListPage(),
             ),
           ],
         ),
       ),
-      body: TabBarView(
-        controller: _tabController,
-        children: const <Widget>[
-          Center(
-            child: FamilyListPage(),
-          ),
-          Center(
-            child: MyListPage(),
-          ),
-        ],
-      ),
     );
+  }
+}
+
+class MyListPage extends StatelessWidget {
+  const MyListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("My List Page");
   }
 }
