dbc021a9f2075d3e9edbe44d2b5de2d782bdb24a
huhuwashere <janakovacikova@protonmail.com>
2023-12-10 20:07:17 +0100

    needed push before pull

--- lib/Pages/BEtestPages/CreateRoomPage.dart
+++ lib/Pages/BEtestPages/CreateRoomPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:itu_app/Database/DataClasses/Room.dart';
 import 'package:itu_app/Database/DatabaseHandler.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class MyCreateRoomPage extends StatefulWidget {
   const MyCreateRoomPage({super.key});
@@ -63,33 +64,7 @@ class _MyCreateRoomPageState extends State<MyCreateRoomPage> {
           ),
           Padding(
             padding: const EdgeInsets.all(10.0),
-            child: Container(
-              height: 400,
-              //width: double.infinity,
-              child: ListWheelScrollView(
-                //scrollDirection: Axis.horizontal,
-                controller: controller,
-                physics: const FixedExtentScrollPhysics(),
-                itemExtent: MediaQuery.of(context).size.width,
-                children: [
-                  Image.asset("res/images/1.png"),
-                  Image.asset("res/images/2.png"),
-                  Image.asset("res/images/3.png"),
-                  Image.asset("res/images/4.png"),
-                  Image.asset("res/images/5.png"),
-                  Image.asset("res/images/6.png"),
-                  Image.asset("res/images/7.png"),
-                  Image.asset("res/images/8.png"),
-                  Image.asset("res/images/9.png"),
-                  Image.asset("res/images/10.png"),
-                  Image.asset("res/images/11.png"),
-                  Image.asset("res/images/12.png"),
-                  Image.asset("res/images/13.png"),
-                  Image.asset("res/images/14.png"),
-                  Image.asset("res/images/15.png"),
-                ]
-              ),
-            ),
+            child: CarouselDemo(),
           ),
           Padding(
             padding: const EdgeInsets.all(10.0),
@@ -115,5 +90,43 @@ class _MyCreateRoomPageState extends State<MyCreateRoomPage> {
       ),
     );
   }
-
 }
+
+class CarouselDemo extends StatelessWidget {
+  CarouselController buttonCarouselController = CarouselController();
+
+  @override
+  Widget build(BuildContext context) => Column(
+      children: <Widget>[
+        CarouselSlider(
+          items: [
+            Image.asset("res/images/1.png"),
+            Image.asset("res/images/2.png"),
+            Image.asset("res/images/3.png"),
+            Image.asset("res/images/4.png"),
+            Image.asset("res/images/5.png"),
+            Image.asset("res/images/6.png"),
+            Image.asset("res/images/7.png"),
+            Image.asset("res/images/8.png"),
+            Image.asset("res/images/9.png"),
+            Image.asset("res/images/10.png"),
+            Image.asset("res/images/11.png"),
+            Image.asset("res/images/12.png"),
+            Image.asset("res/images/13.png"),
+            Image.asset("res/images/14.png"),
+            Image.asset("res/images/15.png"),
+          ],
+          carouselController: buttonCarouselController,
+          options: CarouselOptions(
+            autoPlay: false,
+            enlargeCenterPage: true,
+            viewportFraction: 0.9,
+            aspectRatio: 2.0,
+            initialPage: 2,
+            padEnds: true
+          ),
+        ),
+      ]
+  );
+}
+
